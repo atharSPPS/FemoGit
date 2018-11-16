52c123f31a90acb8c581c1a148cadec75abcac73
atharSPPS <34570080+atharSPPS@users.noreply.github.com>
2018-11-16 17:02:50 +0530

    added FemoGit project to local repo

---
+++ src/main/java/com/athar/FemoGit/App.java
@@ -0,0 +1,13 @@
+package com.athar.FemoGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Athar Khannnnnnn World!" );
+    }
+}
